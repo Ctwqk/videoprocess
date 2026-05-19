ef74dc553d43c711ffbc22a33fa523f1b88a0b92
ctwqk <taiwei293@gmail.com>
2026-05-19 01:02:34 -0700

    feat: add go persistence foundations

---
+++ internal/store/store.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store struct {
+	Pool *pgxpool.Pool
+}
+
+func Open(ctx context.Context, databaseURL string) (*Store, error) {
+	pool, err := pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		return nil, err
+	}
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, err
+	}
+	return &Store{Pool: pool}, nil
+}
+
+func (s *Store) Close() {
+	if s != nil && s.Pool != nil {
+		s.Pool.Close()
+	}
+}
